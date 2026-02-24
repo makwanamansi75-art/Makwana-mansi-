02c37b6cb9288ba599617ef2424796f99db75026
makwanamansi75-art <makwanamansi75@gmail.com>
2026-02-24 12:02:26 +0530

    Add files via upload

---
+++ wrong code.py
@@ -0,0 +1,16 @@
+print("Flower Shop")
+flowers = ["Rose","Lily","Lotus"]
+price = [50,80,100]
+flower = input("Enter flower: ")
+qty = input("Enter qty: ")
+index = flowers.index(flower)
+total = price[index] * qty
+if total > 300
+ discount = total * 0.05
+else
+ discount = 0
+print("Flower:", flower)
+print("Total:", Total)
+for i in range(3)
+print("Sold")
+print("End")
